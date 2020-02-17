389d1b45ff3bac2bee68bce89350f6c61f7039e8
Pablo Ponce <47292039+pablosponce@users.noreply.github.com>
2020-02-17 20:23:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//Load express module with `require` directive
+var express = require('express')
+var app = express()
+
+//Define request response in root URL (/)
+app.get('/', function (req, res) {
+  res.send('Parece que funciona!')
+})
+
+//Launch listening server on port 3000
+app.listen(3000, function () {
+  console.log('Otro ejercicio conseguido!')
+})
